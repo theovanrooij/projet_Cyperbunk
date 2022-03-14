7693963268dd89be1d4eeead205a82e79226e80e
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-03-14 21:40:12 +0100

    deleting my test files

--- feels_analysis/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-### vectorizer
-tfidf_ngrams = TfidfVectorizer(
-    ngram_range=(1, 2), analyzer="word", binary=False)
-tfidf_ngrams.fit(X)
-X_vec = tfidf_ngrams.transform(X)

2d890b255622fcde7731670011aa75ce92a4ab9e
Méline Dang <84090602+MelineDang@users.noreply.github.com>
2022-04-05 18:51:18 +0200

    flaskapp

---
+++ flaskapp/exemple/graphsforums.py
@@ -0,0 +1,19 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title': 'CyberpunkForum',
+            'body': 'haha'
+        },
+        {
+            'id':2,
+            'title': 'CyberpunkFoazeazearum',
+            'body': 'haha'
+        },
+        {
+            'id':3,
+            'title': 'CyberpunkFozaeazem',
+            'body': 'haha'
+        }
+    ]
+    return articles
---
+++ flaskapp/exemple/hello.py
@@ -0,0 +1,27 @@
+from flask import Flask, flash, send_from_directory,redirect, render_template, request, url_for
+from graphsforums import Articles
+app = Flask(__name__)
+
+Articles = Articles()
+
+#   ### Gestion des pages web
+
+@app.route('/')
+def accueil():
+    return render_template('accueil.html')
+
+@app.route('/etude-cyberpunk')
+def etude_cyberpunk():
+    return render_template('etude_cyberpunk.html', articles = Articles )
+
+@app.route('/sous-genres')
+def sous_genres():
+    return render_template('sous_genres.html')
+
+@app.route('/autres-genres')
+def autres_genres():
+    return render_template('autres_genres.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) #pour que les changements se mettent à jour en théorie??
+    
